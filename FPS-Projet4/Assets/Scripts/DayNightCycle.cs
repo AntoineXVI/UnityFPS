09c73b56dcb5a9bfd356d03199ad0376a2d38638
SummerRainyAppleHugo <hugo.h.xia@gmail.com>
2024-11-13 01:14:36 +0100

    DayNightCycle Completed
    Need to Change Environment Material Emission (And it's done)
    (Merge Finally DONE !)

--- FPS-Projet4/Assets/Scripts/DayNightCycle.cs
+++ FPS-Projet4/Assets/Scripts/DayNightCycle.cs
@@ -1,19 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
 
     public float EnvironmentIntensity1 = 3.0f;
-    public float EnvironmentIntensity2 = 10.0f;
-    public float dayTime = 30.0f;
+    public float EnvironmentIntensityStart = 10.0f;
+    public float dayTime = 6.0f;
+    public float dayTick = 600f;
+    public float timeTick = 0.0f;
 
 
-    bool start;
-    bool darken;
-    bool lighten;
+    public bool start;
+    public bool darken = true;
+    public bool lighten = false;
+    public bool intensity = false;
 
     GameObject sunMoon;
     GameObject environmentLighting;
@@ -21,7 +25,7 @@ public class DayNightCycle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        RenderSettings.ambientIntensity = EnvironmentIntensity2;
+        RenderSettings.ambientIntensity = EnvironmentIntensityStart;
         start = true;
     }
 
@@ -31,31 +35,52 @@ public class DayNightCycle : MonoBehaviour
 
         if (RenderSettings.ambientIntensity > EnvironmentIntensity1 && start == true)
         {
-            RenderSettings.ambientIntensity -= 0.02f;
-        }
-        if (RenderSettings.ambientIntensity == EnvironmentIntensity1)
-        {
-            start = false;
+            RenderSettings.ambientIntensity -= 0.01f;
+            if (RenderSettings.ambientIntensity == EnvironmentIntensity1)
+            {
+                start = false;
 
+            }
         }
         else
         {
-
             sunMoon = GameObject.FindWithTag("SunMoon");
-            sunMoon.transform.Rotate(0.0f, 5f / dayTime, 0.0f);
-            if (RenderSettings.ambientIntensity >= 0 && darken == true)
-            {
-                RenderSettings.ambientIntensity -= 0.1f / dayTime;
-                lighten = true;
-                darken = false;
-            }
-            else if (RenderSettings.ambientIntensity <= 0 && lighten == true)
-            {
+            sunMoon.transform.Rotate(0.0f, 0.3f / dayTime, 0.0f);
+            //Task.Delay(TimeSpan.FromSeconds(30)).Wait();
+            //Task.Delay(1000).ContinueWith(t=> //CodeDeLaSuite);
 
-                
+            if (timeTick < dayTime * dayTick)
+            {
+                timeTick++;
             }
-            //environmentLighting = GameObject.FindWithTag("Environment");
-            //environmentLighting.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(0,0,0) * (0.1f));
+            else
+            {
+                intensity = true;
+                timeTick = 0f;
+            }
+
+            if (RenderSettings.ambientIntensity >= 0 && darken == true && intensity == true)
+            {
+                RenderSettings.ambientIntensity -= 0.01f;
+                if (RenderSettings.ambientIntensity <= 0f)
+                {
+                    darken = false;
+                    lighten = true;
+                    intensity = false;
+                }
+            }
+            if (RenderSettings.ambientIntensity <= EnvironmentIntensity1 && lighten == true && intensity == true)
+            {
+                RenderSettings.ambientIntensity += 0.01f;
+                if (RenderSettings.ambientIntensity >= EnvironmentIntensity1)
+                {
+                    //environmentLighting = GameObject.FindWithTag("Environment");
+                    //environmentLighting.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(0, 0, 0) * (-5f));
+                    darken = true;
+                    lighten = false;
+                    intensity = false;
+                }
+            }
         }
     }
 }
